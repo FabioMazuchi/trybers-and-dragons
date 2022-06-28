5ef204b0d7ff0a39791808381916d5f48bc4a2eb
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-06-28 17:54:58 -0300

    requisito 07

--- src/Fighter/index.ts
+++ src/Fighter/index.ts
@@ -1,5 +1,10 @@
+// import Character from '../Character';
 import Fighter from './Fighter';
 import SimpleFighter from './SimpleFighter';
+
+// const enemy = new Character('Inimigo');
+// const character = new Character('Bob Guerreiro');
+// console.log(character.energy.amount = 3);
 
 export default Fighter;
 export { SimpleFighter };
