df43b91f47e53a8ee8fbd490665d5f0762205f37
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-06-28 18:37:48 -0300

    ajustes

--- src/Fighter/index.ts
+++ src/Fighter/index.ts
@@ -1,10 +1,5 @@
-// import Character from '../Character';
 import Fighter from './Fighter';
 import SimpleFighter from './SimpleFighter';
-
-// const enemy = new Character('Inimigo');
-// const character = new Character('Bob Guerreiro');
-// console.log(character.energy.amount = 3);
 
 export default Fighter;
 export { SimpleFighter };
